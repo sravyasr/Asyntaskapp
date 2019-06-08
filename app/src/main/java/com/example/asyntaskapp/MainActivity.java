291f0705d1adfd323d3247deb067cda3617bd48e
Sravya <sajjaratnasravya@gmail.com>
2019-06-08 12:42:29 +0530

    downloadtest_commentsforid

--- app/src/main/java/com/example/asyntaskapp/MainActivity.java
+++ app/src/main/java/com/example/asyntaskapp/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get id from xml file using findviewbyid
         img=findViewById(R.id.img);
     }
 
